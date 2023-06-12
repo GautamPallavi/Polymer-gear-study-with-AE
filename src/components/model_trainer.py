88d4d9e77295a4126804206a6b616467a8f1d3cc
Pallavi Gautam <pallavigautam1405@gmail.com>
2023-06-12 15:11:06 +0530

    pulling from origin

---
+++ src/components/model_trainer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import sklearn as sns
+import sklearn.datasets
+import sklearn.ensemble
+import sklearn.model_selection
+from tensorflow.keras.callbacks import EarlyStopping
+import tensorflow as tf
+from tensorflow import keras
+
+##df=pd.read_csv('pp_features.csv')
+
